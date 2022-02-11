173c5c3ef60bff988483562cf170e11e8419493f
Łukasz Langa <lukasz@langa.pl>
2022-02-11 11:14:38 +0100

    Allow running deployment as an entrypoint sub-command; support RSA private keys

--- scripts/docker-entrypoint-clabot.py
+++ scripts/docker-entrypoint-clabot.py
@@ -9,6 +9,8 @@ import ctypes
 import ctypes.util
 import os
 import signal
+import sys
+from urllib.parse import urlparse
 
 from edb_healthcheck import healthcheck
 from mv import Minivisor
@@ -23,10 +25,50 @@ def ensure_dead_with_parent():
     libc.prctl(PR_SET_PDEATHSIG, signal.SIGKILL)
 
 
+def untangle_github_rsa_private_key() -> None:
+    """Heroku sort of supports multiline config vars. Docker .env does not."""
+
+    GITHUB_RSA_PRIVATE_KEY_SOURCE = os.environ["GITHUB_RSA_PRIVATE_KEY_SOURCE"]
+    GITHUB_RSA_KEY_PATH = "/home/github-private-key.pem"
+    os.environ["GITHUB_RSA_PRIVATE_KEY"] = GITHUB_RSA_KEY_PATH
+
+    with open(GITHUB_RSA_KEY_PATH, "w") as key_file:
+        source = GITHUB_RSA_PRIVATE_KEY_SOURCE
+        if source[0] == '"':
+            import ast
+            parsed = ast.parse(source, mode="eval").body
+            if isinstance(parsed, ast.Constant):
+                # Python 3.9+
+                source = parsed.value
+            elif isinstance(parsed, ast.Str):
+                # Python 3.7
+                source = parsed.s
+            else:
+                raise TypeError(f"{type(parsed)}")
+        key_file.write(source)
+
+
 async def main() -> None:
     PORT = os.environ["PORT"]
     DATABASE_URL = os.environ["DATABASE_URL"]
+    EDGEDB_PASSWORD = urlparse(DATABASE_URL).password
     mv = Minivisor()
+
+    new_release = False
+    if len(sys.argv) > 1:
+        if sys.argv[1] == "deployment":
+            await mv.out.put(b"Running deployment tasks for a new release...")
+            new_release = True
+        else:
+            await mv.out.put(b"Invalid entrypoint sub-command: " + sys.argv[1].encode())
+            await mv.shutdown()
+            return
+
+    if new_release:
+        import deployment
+        await deployment.main(mv)
+        return
+
     await mv.spawn(
         "edgedb-server",
         "--bind-address=0.0.0.0",
@@ -34,10 +76,18 @@ async def main() -> None:
         "--tls-cert-mode=generate_self_signed",
         f"--backend-dsn={DATABASE_URL}",
         with_healthcheck=healthcheck,
-        grace_period=120.0,  # some grace for bootstrapping
+        grace_period=20.0,
     )
 
-    # await mv.spawn("yarn", "next", "start", "-p", PORT)
+    os.environ["EDGEDB_HOST"] = "127.0.0.1"
+    os.environ["EDGEDB_PORT"] = "5656"
+    os.environ["EDGEDB_PASSWORD"] = EDGEDB_PASSWORD
+    os.environ["ORGANIZATION_NAME"] = "python"
+    os.environ["ORGANIZATION_DISPLAY_NAME"] = "Python Software Foundation"
+
+    untangle_github_rsa_private_key()
+
+    await mv.spawn("yarn", "next", "start", "-p", PORT)
     await mv.wait_until_any_terminates()
 
 
